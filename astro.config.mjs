58e4b9f62f8f23069da73867494c69124ec771c5
Hangtime265 <matt_hanger@hotmail.com>
2023-04-19 16:31:43 +1000

    Changes to Css and layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://matt-hanger.github.io/',
+  site: 'https://matt-hanger.github.io/',
+  integrations: [tailwind()]
 });
